8cd7375bac060111dc3c93c054415cdf77a23135
Jayakumar <jayakumar.m.h@gmail.com>
2021-08-24 23:02:53 +0100

    account object added

--- .sfdx/typings/lwc/apex/AccountController.d.ts
+++ .sfdx/typings/lwc/apex/AccountController.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/AccountController.findAccounts" {
   export default function findAccounts(param: {searchKey: any}): Promise<any>;
 }
+declare module "@salesforce/apex/AccountController.findAccounts1" {
+  export default function findAccounts1(param: {searchKey: any}): Promise<any>;
+}
